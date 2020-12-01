c911a1e4ec3165e40b95f0521fde29b84db84948
eviadadon <ad9372@bs.amalnet.k12.il>
2020-12-01 20:31:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/eviaalpha/Authtintication.java
@@ -0,0 +1,36 @@
+package com.example.eviaalpha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Authtintication extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authtintication);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+    }
+
+    public void signyp(View view) {
+    }
+
+    public void log(View view) {
+    }
+}
