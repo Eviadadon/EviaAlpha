2c74aadaa2b203147bea10f9b9217234640cb71f
eviadadon <ad9372@bs.amalnet.k12.il>
2020-12-30 16:15:55 +0200

    f

--- app/src/main/java/com/example/eviaalpha/Authtintication.java
+++ app/src/main/java/com/example/eviaalpha/Authtintication.java
@@ -1,36 +1,96 @@
 package com.example.eviaalpha;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Authtintication extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    EditText email;
+    EditText passw;
+    Button login;
+    Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authtintication);
         mAuth = FirebaseAuth.getInstance();
-    }
+        email= findViewById(R.id.edittextmail);
+        passw= findViewById(R.id.edittextpassw);
+        login= findViewById(R.id.button9999);
+        signup = findViewById(R.id.buton999);
 
+    }
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
+        updateUI(currentUser); }
 
     private void updateUI(FirebaseUser currentUser) {
     }
 
-    public void signyp(View view) {
+    public void login(View view) {
+        String emailchack= email.getText().toString();
+        String passwordcheck = passw.getText().toString();
+        mAuth.signInWithEmailAndPassword(emailchack, passwordcheck)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(Authtintication.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
 
-    public void log(View view) {
+    public void signup(View view) {
+        String emailnew= email.getText().toString();
+        String password = passw.getText().toString();
+        mAuth.createUserWithEmailAndPassword(emailnew, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(Authtintication.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
     }
 }
